9a0f14d6db60d09a92718e811b10c82b9ad27e10
Kreisemann <86150229+Kreisemann@users.noreply.github.com>
2023-09-08 09:19:09 +0200

    V1.1.1

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+#Kommentare mit hash-tag
+text1 = "variablen werden nur benannt. Die art der Variable wird automatisch bestimmt"
+number1 = 9
+number2 = 3
+number3 = number1 + number2
+#creating functions:
+def my_function():
+    print(text1)
+#calling functions:
+my_function()
+#other example:
+def damn(name):
+    print(name + "ist sehr genial")
+
+damn("Magnus")
